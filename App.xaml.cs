914d6294c3ad0ca139409092db78816fede9159f
walther salas <wasalas@hotmail.com>
2023-03-15 10:49:41 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using ProsperDaily.MVVM.Models;
+using ProsperDaily.MVVM.Views;
+using ProsperDaily.Repositories;
+
+namespace ProsperDaily;
+
+public partial class App : Application
+{
+    public static BaseRepository<Transaction>
+        TransactionsRepo
+    { get; private set; }
+    public App(BaseRepository<Transaction> _transactionsRepo)
+    {
+        InitializeComponent();
+        TransactionsRepo = _transactionsRepo;
+
+        MainPage = new NavigationPage(new DashboardPage());
+        //MainPage = new TransactionsPage();
+    }
+}
